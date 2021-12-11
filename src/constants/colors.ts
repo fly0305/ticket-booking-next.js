b83d52ec432dc35224fd8e4742babaa820822bc4
flyasher <flyhighasher@gmail.com>
2021-12-11 11:12:31 +0530

    Modifed folder structure

---
+++ src/constants/colors.ts
@@ -0,0 +1,27 @@
+const colors = {
+  primary: "#fca311",
+};
+
+const sizes = {
+  // global sizes
+  fontXS: 10,
+  fontS: 12,
+  font: 14,
+  fontL: 16,
+  fontXL: 18,
+  fontXXL: 20,
+  fontXXXL: 22,
+  radius: 6,
+  padding: 10,
+
+  // font sizes
+  h1: 26,
+  h2: 20,
+  h3: 18,
+  title: 18,
+  header: 16,
+  body: 14,
+  caption: 12
+}
+
+export { colors, sizes };
---
+++ src/pages/tickets.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const Tickets = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Tickets</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>Tickets</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      </div>
+    </>
+  );
+}
+ 
+export default Tickets;
